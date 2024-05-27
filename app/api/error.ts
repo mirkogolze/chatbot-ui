cac7d043a0be792d346e3a2b90469c340d3bc3da
Mirko Golze <mirko.golze@t-systems.com>
2024-05-27 07:37:41 +0200

    formattings

--- app/api/error.ts
+++ app/api/error.ts
@@ -1,15 +1,15 @@
 export default function Error({
-    error,
-  }: {
-    error: Error & { digest?: string }
-  }) {
-    const errorMessage = error.message || "An unexpected error occurred"
-    //const errorCode = error?.status || 500
-    //return new Response(JSON.stringify({ message: errorMessage }), {
-    //  status: errorCode
-    //})
-    console.error(error);
-    return new Response(JSON.stringify({ message: errorMessage }), {
-       status: 500
-      })
-  }
+  error
+}: {
+  error: Error & { digest?: string }
+}) {
+  const errorMessage = error.message || "An unexpected error occurred"
+  //const errorCode = error?.status || 500
+  //return new Response(JSON.stringify({ message: errorMessage }), {
+  //  status: errorCode
+  //})
+  console.error(error)
+  return new Response(JSON.stringify({ message: errorMessage }), {
+    status: 500
+  })
+}
--- app/api/retrieval/retrieve/route.ts
+++ app/api/retrieval/retrieve/route.ts
@@ -23,141 +23,141 @@ export async function POST(request: Request) {
 
   const uniqueFileIds = [...new Set(fileIds)]
   const uniqueVectorNames = [...new Set(vectorNames)]
- // try {
-    const supabaseAdmin = createClient<Database>(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.SUPABASE_SERVICE_ROLE_KEY!
-    )
+  // try {
+  const supabaseAdmin = createClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!
+  )
 
-    const profile = await getServerProfile()
+  const profile = await getServerProfile()
 
-    if (embeddingsProvider === "openai") {
-      if (profile.use_azure_openai) {
-        checkApiKey(profile.azure_openai_api_key, "Azure OpenAI")
-      } else {
-        checkApiKey(profile.openai_api_key, "OpenAI")
-      }
-    }
-
-    let chunks: any[] = []
-
-    let openai
+  if (embeddingsProvider === "openai") {
     if (profile.use_azure_openai) {
-      openai = new OpenAI({
-        apiKey: profile.azure_openai_api_key || "",
-        baseURL: `${profile.azure_openai_endpoint}/openai/deployments/${profile.azure_openai_embeddings_id}`,
-        defaultQuery: { "api-version": "2023-12-01-preview" },
-        defaultHeaders: { "api-key": profile.azure_openai_api_key }
-      })
+      checkApiKey(profile.azure_openai_api_key, "Azure OpenAI")
     } else {
-      openai = new OpenAI({
-        apiKey: profile.openai_api_key || "",
-        organization: profile.openai_organization_id
-      })
+      checkApiKey(profile.openai_api_key, "OpenAI")
     }
+  }
 
-    if (embeddingsProvider === "openai") {
-      const response = await openai.embeddings.create({
-        model: "text-embedding-3-small",
-        input: userInput
-      })
-      const openaiEmbedding = response.data.map(item => item.embedding)[0]
-      if (process.env.EMBEDDING_STORAGE == "qdrant") {
-        const qclient = new qDrant()
-        chunks = await qclient.searchEmbeddings(
-          uniqueFileIds,
-          uniqueVectorNames,
-          profile.user_id,
-          openaiEmbedding,
-          embeddingsProvider
-        )
-      } else {
-        const { data: openaiFileItems, error: openaiError } =
-          await supabaseAdmin.rpc("match_file_items_openai", {
-            query_embedding: openaiEmbedding as any,
-            match_count: sourceCount,
-            file_ids: uniqueFileIds
-          })
+  let chunks: any[] = []
 
-        if (openaiError) {
-          throw openaiError
-        }
-        chunks = openaiFileItems
+  let openai
+  if (profile.use_azure_openai) {
+    openai = new OpenAI({
+      apiKey: profile.azure_openai_api_key || "",
+      baseURL: `${profile.azure_openai_endpoint}/openai/deployments/${profile.azure_openai_embeddings_id}`,
+      defaultQuery: { "api-version": "2023-12-01-preview" },
+      defaultHeaders: { "api-key": profile.azure_openai_api_key }
+    })
+  } else {
+    openai = new OpenAI({
+      apiKey: profile.openai_api_key || "",
+      organization: profile.openai_organization_id
+    })
+  }
+
+  if (embeddingsProvider === "openai") {
+    const response = await openai.embeddings.create({
+      model: "text-embedding-3-small",
+      input: userInput
+    })
+    const openaiEmbedding = response.data.map(item => item.embedding)[0]
+    if (process.env.EMBEDDING_STORAGE == "qdrant") {
+      const qclient = new qDrant()
+      chunks = await qclient.searchEmbeddings(
+        uniqueFileIds,
+        uniqueVectorNames,
+        profile.user_id,
+        openaiEmbedding,
+        embeddingsProvider
+      )
+    } else {
+      const { data: openaiFileItems, error: openaiError } =
+        await supabaseAdmin.rpc("match_file_items_openai", {
+          query_embedding: openaiEmbedding as any,
+          match_count: sourceCount,
+          file_ids: uniqueFileIds
+        })
+
+      if (openaiError) {
+        throw openaiError
       }
-    } else if (embeddingsProvider === "local") {
-      const localEmbedding = await generateLocalEmbedding(userInput)
-
-      if (process.env.EMBEDDING_STORAGE == "qdrant") {
-        const qclient = new qDrant()
-        chunks = await qclient.searchEmbeddings(
-          uniqueFileIds,
-          uniqueVectorNames,
-          profile.user_id,
-          localEmbedding,
-          embeddingsProvider
-        )
-      } else {
-        const { data: localFileItems, error: localFileItemsError } =
-          await supabaseAdmin.rpc("match_file_items_local", {
-            query_embedding: localEmbedding as any,
-            match_count: sourceCount,
-            file_ids: uniqueFileIds
-          })
-
-        if (localFileItemsError) {
-          throw localFileItemsError
-        }
-        chunks = localFileItems
-      }
-    } else if (
-      embeddingsProvider === "multilingual-e5-large" ||
-      embeddingsProvider === "multilingual-e5-small"
-    ) {
-      const customOpenai = new OpenAI({
-        baseURL: process.env.OPENAI_BASE_URL,
-        apiKey: "DUMMY"
-      })
-      const response = await customOpenai.embeddings.create({
-        model: embeddingsProvider,
-        input: userInput
-      })
-
-      const openaiEmbedding = response.data.map(item => item.embedding)[0]
-      if (process.env.EMBEDDING_STORAGE == "qdrant") {
-        const qclient = new qDrant()
-        chunks = await qclient.searchEmbeddings(
-          uniqueFileIds,
-          uniqueVectorNames,
-          profile.user_id,
-          openaiEmbedding,
-          embeddingsProvider
-        )
-      } else {
-        const { data: openaiFileItems, error: openaiError } =
-          await supabaseAdmin.rpc("match_file_items_openai", {
-            query_embedding: openaiEmbedding as any,
-            match_count: sourceCount,
-            file_ids: uniqueFileIds
-          })
-        if (openaiError) {
-          throw openaiError
-        }
-        chunks = openaiFileItems
-      }
+      chunks = openaiFileItems
     }
+  } else if (embeddingsProvider === "local") {
+    const localEmbedding = await generateLocalEmbedding(userInput)
+
+    if (process.env.EMBEDDING_STORAGE == "qdrant") {
+      const qclient = new qDrant()
+      chunks = await qclient.searchEmbeddings(
+        uniqueFileIds,
+        uniqueVectorNames,
+        profile.user_id,
+        localEmbedding,
+        embeddingsProvider
+      )
+    } else {
+      const { data: localFileItems, error: localFileItemsError } =
+        await supabaseAdmin.rpc("match_file_items_local", {
+          query_embedding: localEmbedding as any,
+          match_count: sourceCount,
+          file_ids: uniqueFileIds
+        })
+
+      if (localFileItemsError) {
+        throw localFileItemsError
+      }
+      chunks = localFileItems
+    }
+  } else if (
+    embeddingsProvider === "multilingual-e5-large" ||
+    embeddingsProvider === "multilingual-e5-small"
+  ) {
+    const customOpenai = new OpenAI({
+      baseURL: process.env.OPENAI_BASE_URL,
+      apiKey: "DUMMY"
+    })
+    const response = await customOpenai.embeddings.create({
+      model: embeddingsProvider,
+      input: userInput
+    })
+
+    const openaiEmbedding = response.data.map(item => item.embedding)[0]
+    if (process.env.EMBEDDING_STORAGE == "qdrant") {
+      const qclient = new qDrant()
+      chunks = await qclient.searchEmbeddings(
+        uniqueFileIds,
+        uniqueVectorNames,
+        profile.user_id,
+        openaiEmbedding,
+        embeddingsProvider
+      )
+    } else {
+      const { data: openaiFileItems, error: openaiError } =
+        await supabaseAdmin.rpc("match_file_items_openai", {
+          query_embedding: openaiEmbedding as any,
+          match_count: sourceCount,
+          file_ids: uniqueFileIds
+        })
+      if (openaiError) {
+        throw openaiError
+      }
+      chunks = openaiFileItems
+    }
+  }
 
     const mostSimilarChunks = chunks?.sort(
       (a, b) => b.similarity - a.similarity
     )
-    return new Response(JSON.stringify({ results: mostSimilarChunks }), {
-      status: 200
-    })
+  return new Response(JSON.stringify({ results: mostSimilarChunks }), {
+    status: 200
+  })
   //} catch (error: any) {
   //  const errorMessage = error.error?.message || "An unexpected error occurred"
   //  const errorCode = error.status || 500
-    //return new Response(JSON.stringify({ message: errorMessage }), {
-    //  status: errorCode
-    //})
+  //return new Response(JSON.stringify({ message: errorMessage }), {
+  //  status: errorCode
+  //})
   //  console.error(error);
   //  return new Response(JSON.stringify({ message: errorMessage }), {
   //     status: errorCode
